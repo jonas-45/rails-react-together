05b248131d524e90b4f1ef607ec05ae0b4009862
jonas-45 <jonaswullah@gmail.com>
2023-09-07 23:28:02 +0000

    Add api to fetch random greeting

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,4 @@
-class Greeting < ApplicationRecord
+class GreetingsController < ApplicationController
   def greeting
     @greeting = Greeting.order('RANDOM()').first
     render json: {greeting: @greeting.greeting}
