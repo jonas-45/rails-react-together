fa1f91e648e6c2e06a8e405519de3a3011c8b380
jonas-45 <jonaswullah@gmail.com>
2023-09-07 23:43:28 +0000

    Add greeting store and slice for state management

---
+++ app/javascript/redux/greeting/greetingSlice.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getRandomGreeting = createAsyncThunk(
+  'greetings/random',
+  async () => {
+    try {
+      const res = await fetch('/greetings/greeting');
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await res.json();
+      return data.greeting;
+    } catch (error) {
+      console.error('Error fetching greeting:', error);
+      throw error; // Rethrow the error to be caught by the component or further handled
+    }
+  },
+);
+
+const initialState = {
+  message: '',
+};
+
+const greetingSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  extraReducers(builder) {
+    builder.addCase(getRandomGreeting.fulfilled, (state, action) => {
+      state.message = action.payload;
+    });
+  },
+});
+
+export default greetingSlice.reducer;
