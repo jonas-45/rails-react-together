0a535ef623650d56c5f404710e346ee131e810dc
jonas-45 <jonaswullah@gmail.com>
2023-09-08 16:26:25 +0000

    Add linters configuration

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def greeting
     @greeting = Greeting.order('RANDOM()').first
-    render json: {greeting: @greeting.greeting}
+    render json: { greeting: @greeting.greeting }
   end
 end
